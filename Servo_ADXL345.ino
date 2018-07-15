7efc8e3e428918a4456c5ac3733080a407bccd0e
manzoor11 <manzoor11@gmail.com>
2018-07-15 12:01:58 +0530

    Add files via upload

---
+++ Servo_ADXL345.ino
@@ -0,0 +1,177 @@
+ // Include the Servo library
+#include <Servo.h> 
+#include <ADXL345.h>
+
+ADXL345 accel;
+char hs;
+
+// Declare the Servo & Switch pin
+int servoPin1 = 3;
+int servoPin2 = 4;
+int servoPin3 = 5;
+
+const int s1Pin = 8;
+const int s2Pin = 9;
+const int s3Pin = 10;
+int s1State = 0;
+int s2State = 0;
+int s3State = 0;
+int pos1 = 0;    // variable to store the servo1 position
+int pos2 = 0;    // variable to store the servo2 position
+int pos3 = 0;    // variable to store the servo3 position
+
+// Create a servo object
+Servo Servo1;
+Servo Servo2;
+Servo Servo3;
+
+void setup() {
+  // initialize the pushbutton pin as an input:
+  pinMode(s1Pin, INPUT);
+  pinMode(s2Pin, INPUT);
+  pinMode(s3Pin, INPUT);
+  
+  Serial.begin(9600);
+  Wire.begin();
+  // We need to attach the servo to the used pin number
+  Servo1.attach(servoPin1);
+  Servo2.attach(servoPin2);
+  Servo3.attach(servoPin3);
+  // Caliberate Accilerometer
+  accel.init(-1, 0, 8);
+  accel.setSoftwareOffset(-0.023, 0, 0.03577027);
+  accel.printCalibrationValues(40);
+
+    // Make servo go to initial position : 90 degree in code and 0 degree wrt reference
+
+    Servo1.write(86);
+    delay(500);
+
+    Servo2.write(4);
+    delay(500);
+
+    Servo3.write(90);
+    delay(500);
+}
+
+void loop(){
+  
+int i;
+float pitch=0, roll = 0;
+    /*  hs = 0;
+         while(hs != '1')
+            {
+              hs = Serial.read();
+            } */
+            
+      //Aquire data
+     // AccelG accelG;
+     // accelG = accel.readAccelG();
+    
+    for(i=1;i<100;i++)
+    {  
+      AccelRotation accelRot;
+      accelRot = accel.readPitchRoll();
+      if(accelRot.pitch >= 0)
+      {
+        pitch = ((pitch + accelRot.pitch)/2);
+      }
+      if(accelRot.roll >= (-90))
+      {
+        roll = ((roll + accelRot.roll)/2);
+      }
+     }
+    int pAngle = pitch;
+    int rAngle = roll;
+ 
+   //     int a = (pAngle/2.5);               // for rounding the pitch angle to near by value which is multiple of 5s
+   //     pAngle = ((( a/2)*5)+((a%2)*5));
+
+   //     int b = (rAngle/2.5);               // for rounding the roll angle to near by value which is multiple of 5s
+   //     rAngle = ((( b/2)*5)+((b%2)*5));       
+   
+     // read the state of the pushbutton value:
+    s1State = digitalRead(s1Pin);
+    s2State = digitalRead(s2Pin);
+    s3State = digitalRead(s3Pin); 
+    
+    if(s1State == LOW )
+    {
+    Serial.print(" 1 : "); 
+    Serial.println(pAngle);
+
+      pAngle = ( 86 - pAngle );
+    
+    pos1 = Servo1.read();
+      if(pAngle>pos1)
+      {
+        for (i = pos1; i <= pAngle; i++)  
+        {
+          Servo1.write(i); 
+          delay(15);
+        } 
+      }
+      else if(pAngle<pos1)
+      {
+        for (i = pos1; i >= pAngle; i--)  
+        {
+          Servo1.write(i); 
+          delay(15);
+        }      
+      }
+    }
+    else if(s2State == LOW)
+    {
+
+    
+    Serial.print(" 2 : ");
+    Serial.println(pAngle);
+     
+      pAngle = (4 + pAngle);
+
+    pos2 = Servo2.read();
+      if(pAngle>pos2)
+      {
+        for (i = pos2; i <= pAngle; i++)   
+        {
+          Servo2.write(i); 
+          delay(15);
+        } 
+      }
+      else if(pAngle<pos2)
+      {
+        for (i = pos2; i >= pAngle; i--)  
+        {
+          Servo2.write(i); 
+          delay(15);
+        }      
+      }
+    }
+
+    else if(s3State == LOW)
+    {  
+    Serial.print(" 3 : ");
+    Serial.println(rAngle);
+     
+      rAngle = (90 + rAngle);
+
+    pos3 = Servo3.read();
+      if(rAngle>pos3)
+      {
+        for (i = pos3; i <= rAngle; i++)   
+        {
+          Servo3.write(i); 
+          delay(15);
+        } 
+      }
+      else if(rAngle<pos3)
+      {
+        for (i = pos3; i >= rAngle; i--)  
+        {
+          Servo3.write(i); 
+          delay(15);
+        }      
+      }
+    }    
+}
+
---
+++ robo_AD_LV_2/robo_AD_LV_2.ino
@@ -0,0 +1,219 @@
+ // Include the Servo library
+#include <Servo.h> 
+#include <ADXL345.h>
+
+ADXL345 accel;
+char hs;
+
+// Declare the Servo & Switch pin
+int servoPin1 = 3;
+int servoPin2 = 4;
+int servoPin3 = 5;
+
+const int s1Pin = 8;
+const int s2Pin = 9;
+const int s3Pin = 10;
+int s1State = 0;
+int s2State = 0;
+int s3State = 0;
+int pos1 = 0;    // variable to store the servo1 position
+int pos2 = 0;    // variable to store the servo2 position
+int pos3 = 0;    // variable to store the servo3 position
+
+// Create a servo object
+Servo Servo1;
+Servo Servo2;
+Servo Servo3;
+
+void setup() {
+  
+  //look of tap movement on this axes - 1 == on; 0 == off
+  accel.setTapDetectionOnX(0);
+  accel.setTapDetectionOnY(0);
+  accel.setTapDetectionOnZ(1);
+ 
+  //set values for what is a tap, and what is a double tap (0-255)
+  accel.setTapThreshold(50); //62.5mg per increment
+  accel.setTapDuration(15); //625μs per increment
+  accel.setDoubleTapLatency(80); //1.25ms per increment
+  accel.setDoubleTapWindow(200); //1.25ms per increment
+
+    //setting all interupts to take place on int pin 1
+  //I had issues with int pin 2, was unable to reset it
+  accel.setInterruptMapping( ADXL345_INT_SINGLE_TAP_BIT,   ADXL345_INT1_PIN );
+  accel.setInterruptMapping( ADXL345_INT_DOUBLE_TAP_BIT,   ADXL345_INT1_PIN );
+ 
+  //register interupt actions - 1 == on; 0 == off  
+  accel.setInterrupt( ADXL345_INT_SINGLE_TAP_BIT, 1);
+  accel.setInterrupt( ADXL345_INT_DOUBLE_TAP_BIT, 1);
+  
+  // initialize the pushbutton pin as an input:
+  pinMode(s1Pin, INPUT);
+  pinMode(s2Pin, INPUT);
+  pinMode(s3Pin, INPUT);
+  
+  Serial.begin(9600);
+  Wire.begin();
+  // We need to attach the servo to the used pin number
+  Servo1.attach(servoPin1);
+  Servo2.attach(servoPin2);
+  Servo3.attach(servoPin3);
+  // Caliberate Accilerometer
+  accel.init(-1, 0, 8);
+  accel.setSoftwareOffset(-0.023, 0, 0.03577027);
+ // accel.printCalibrationValues(40);
+
+    // Make servo go to initial position : 90 degree in code and 0 degree wrt reference
+
+    Servo1.write(86);
+    delay(500);
+
+    Servo2.write(4);
+    delay(500);
+
+    Servo3.write(90);
+    delay(500);
+}
+
+void loop(){
+  
+int i;
+float pitch=0, roll = 0;
+    /*  hs = 0;
+         while(hs != '1')
+            {
+              hs = Serial.read();
+            } */
+            
+      //Aquire data
+     // AccelG accelG;
+     // accelG = accel.readAccelG();
+    
+    for(i=1;i<100;i++)
+    {  
+      AccelRotation accelRot;
+      accelRot = accel.readPitchRoll();
+      if(accelRot.pitch >= 0)
+      {
+        pitch = ((pitch + accelRot.pitch)/2);
+      }
+      if(accelRot.roll >= (-90))
+      {
+        roll = ((roll + accelRot.roll)/2);
+      }
+     }
+    int pAngle = pitch;
+    int rAngle = roll;
+ 
+        int a = (pAngle/2.5);               // for rounding the pitch angle to near by value which is multiple of 5s
+        pAngle = ((( a/2)*5)+((a%2)*5));
+
+        int b = (rAngle/2.5);               // for rounding the roll angle to near by value which is multiple of 5s
+        rAngle = ((( b/2)*5)+((b%2)*5));       
+
+    char hs = 'N';
+
+     while(hs != 'K')
+        {
+          hs = Serial.read();
+        }
+  
+   
+     // read the state of the pushbutton value:
+    s1State = digitalRead(s1Pin);
+    s2State = digitalRead(s2Pin);
+    s3State = digitalRead(s3Pin); 
+    
+    if(s1State == LOW )
+    {
+      Serial.println("555");  
+    Serial.println('1');
+    Serial.println(pAngle);
+
+      pAngle = ( 86 - pAngle );
+    
+    pos1 = Servo1.read();
+      if(pAngle>pos1)
+      {
+        for (i = pos1; i <= pAngle; i++)  
+        {
+          Servo1.write(i); 
+          delay(15);
+        } 
+      }
+      else if(pAngle<pos1)
+      {
+        for (i = pos1; i >= pAngle; i--)  
+        {
+          Servo1.write(i); 
+          delay(15);
+        }      
+      }
+    }
+    else if(s2State == LOW)
+    {
+      Serial.println("555");
+    Serial.println('2');
+    Serial.println(pAngle);
+     
+      pAngle = (4 + pAngle);
+
+    pos2 = Servo2.read();
+      if(pAngle>pos2)
+      {
+        for (i = pos2; i <= pAngle; i++)   
+        {
+          Servo2.write(i); 
+          delay(15);
+        } 
+      }
+      else if(pAngle<pos2)
+      {
+        for (i = pos2; i >= pAngle; i--)  
+        {
+          Servo2.write(i); 
+          delay(15);
+        }      
+      }
+    }
+
+    else if(s3State == LOW)
+    {
+      Serial.println("555");    
+    Serial.println('3');
+    Serial.println(rAngle);
+     
+      rAngle = (90 + rAngle);
+
+    pos3 = Servo3.read();
+      if(rAngle>pos3)
+      {
+        for (i = pos3; i <= rAngle; i++)   
+        {
+          Servo3.write(i); 
+          delay(15);
+        } 
+      }
+      else if(rAngle<pos3)
+      {
+        for (i = pos3; i >= rAngle; i--)  
+        {
+          Servo3.write(i); 
+          delay(15);
+        }      
+      }
+    }
+
+  //double tap
+  if(accel.triggered(interrupts, ADXL345_DOUBLE_TAP)){
+    Serial.println("double tap");
+     //add code here to do when a 2X tap is sensed
+  }
+  
+  //tap
+  if(accel.triggered(interrupts, ADXL345_SINGLE_TAP)){
+    Serial.println("tap");
+     //add code here to do when a tap is sensed
+  }         
+}
+
